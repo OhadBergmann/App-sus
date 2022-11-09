6ed41ef57e523e5d078561917d7d6c918ab63e38
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-09 23:53:39 +0200

    css mainly - v0.18

--- apps/mail/cmps/mail-filter.cmp.js
+++ apps/mail/cmps/mail-filter.cmp.js
@@ -1,19 +1,37 @@
 import { svgService } from '../services/mail-svg.service.js';
+import searchForm from './search-form.cmp.js'
+
 
 export default {
     template:`
     <section class="mail-filter">
-        <div class="open-menu" v-html="openMenuIcon"></div> 
+        <div class="open-menu circle-animation" v-html="openMenuIcon"></div> 
         <img class="refresh-page" src="./assets/img/mail-logo.png" />
-        <input type="search" class="search-bar"/>
+        <div class="search-bar">
+            <div class="search-icon circle-animation" v-html="searchIcon"></div>
+            <input type="search" class="search-field"/>
+            <div @click="disableAdvanceIcon" class="advance-search circle-animation" :class="{hideme: isHidden}"
+            v-html="advanceIcon"></div>
+            <search-form :class="{hideme: !isHidden}"/>
+        </div> 
     </section>
     `, data(){
         return {
             openMenuIcon: null,
+            searchIcon: null,
+            advanceIcon: null,
+            isHidden: false,
         }
-    },created(){
+    },methods:{
+        disableAdvanceIcon(){
+            this.isHidden = true;
+        }
+    }, created(){
         this.openMenuIcon = svgService.getKeepIcon('bars');
+        this.searchIcon = svgService.getKeepIcon('search');
+        this.advanceIcon = svgService.getKeepIcon('controls');
     },components:{
         svgService,
+        searchForm,
     }
 }
