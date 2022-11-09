4b114798e1fbf179cbb83276145bc6f49ae588b7
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-09 19:19:49 +0200

    v0.15

--- routes.js
+++ routes.js
@@ -31,9 +31,7 @@ const routerOptions = {
                     component: aboutGoals,
                 },                
             ]
-		},
-		},
-		{
+		},{
 			path: '/mail/list',
 			component: mailPage,
 
