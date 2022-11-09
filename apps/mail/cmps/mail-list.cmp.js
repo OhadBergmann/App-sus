257257ace753739c7ae30916cdb446ec22cc0d1e
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-09 13:15:04 +0200

    basic structure - part 2

---
+++ apps/mail/cmps/mail-list.cmp.js
@@ -0,0 +1,14 @@
+export default {
+    template:`
+        <section class="mail-list">
+            <div class="sender"> from</div>
+            <div class="title"> title ?+ subtitle</div>
+            <div class="attach-icon"> attach file icon </div>
+            <div class="mail-date"> date </div>
+        </section>
+    `, data(){
+        return {
+
+        }
+    },
+}
