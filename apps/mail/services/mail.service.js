fe6355cf735699a922f34d42bd7b5b732db577cd
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-09 19:16:25 +0200

    v0.14

--- apps/mail/services/mail.service.js
+++ apps/mail/services/mail.service.js
@@ -18,6 +18,282 @@ function _createMail(){
             subject:'Miss you!',
             body:'Would love to catch up sometimes',
             isRead:false,
+            sentAt:1599133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'VrBQP',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1557458930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'1hvkg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1565133730594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'SJIHg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1588133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'pHh7h',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1577133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'4uSnH',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1574738930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'YfhW3',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1668009111096,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },{
+            id:'Xgm5Ye',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1668009111096,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'VrBQP',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1667009111096,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'1hvkg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'SJIHg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1541133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'pHh7h',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551933930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'4uSnH',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1588133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'YfhW3',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1545433930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },{
+            id:'Xgm5Ye',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1543432930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'VrBQP',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1543232130594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'1hvkg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'SJIHg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'pHh7h',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'4uSnH',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'YfhW3',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },{
+            id:'Xgm5Ye',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'VrBQP',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'1hvkg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'SJIHg',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'pHh7h',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: false
+        },
+        {
+            id:'4uSnH',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },
+        {
+            id:'YfhW3',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
+            sentAt:1551133930594,
+            from:'momo@momo.com',
+            to:'user@appsus.com',
+            hasAttach: true
+        },{
+            id:'Xgm5Ye',
+            subject:'Miss you!',
+            body:'Would love to catch up sometimes',
+            isRead:false,
             sentAt:1551133930594,
             from:'momo@momo.com',
             to:'user@appsus.com',
--- routes.js
+++ routes.js
@@ -33,7 +33,7 @@ const routerOptions = {
             ]
 		},
 		{
-			path: '/mail/inbox',
+			path: '/mail/list',
 			component: mailPage,
 
 		}
