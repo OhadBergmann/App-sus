859f27e72a08cc095ea95ba2cd068adaae353e1f
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-10 10:55:44 +0200

    morning meeting - aftermath

--- apps/mail/cmps/mail-list.cmp.js
+++ apps/mail/cmps/mail-list.cmp.js
@@ -3,20 +3,50 @@ import { clientService } from '../services/mail.service.js'
 
 export default {
 
-    template:`
+    template: `
         <section class="mail-list">
             <mail-preview v-if="mailList && mailList.length > 0" v-for="mail in mailList" :mail="mail"/>
+
+            <button @click="updateTab('inbox')">add Tab</button>
         </section>
-    `, data(){
+    `,
+    data() {
         return {
             mailList: null,
         }
-    },created(){
+    },
+    created() {
+        console.log(this.$route);
+       
         clientService.query()
-        .then(emails=>{
-            this.mailList = emails;
-        });
-    },components:{
-        mailPreview,
+            .then(emails => {
+                this.mailList = emails;
+            });
+    },
+    methods:{
+        updateTab(val){
+            this.$router.push({path:'/mail/list', query:{tab:val}})
+        }
     }
+    ,
+    components: {
+        mailPreview,
+    },
+
+
+    watch: {
+
+        $route(newQuestion, oldQuestion) {
+           console.log(this.$route);
+          },
+        $route: {
+            handler(newValue, oldValue) {
+                console.log(oldValue);
+                console.log(this.$route)
+            },
+            deep: true
+        }
+    }
+
+
 }
