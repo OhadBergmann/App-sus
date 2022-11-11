2131ff9881bf36e1f1a5ee48772993b478888bf7
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-11 21:05:20 +0200

    composing the composer part - 2.1

--- apps/mail/cmps/mail-compose.cmp.js
+++ apps/mail/cmps/mail-compose.cmp.js
@@ -3,14 +3,15 @@ import { svgService } from '../services/mail-svg.service.js';
 export default { 
     props:['isOpen'],
     template:`
-        <section class="" >
+        <section class="" ref="composerContainer">
             <header class="new-mail-header">
                 <label>New Message</label>
                 <div class="new-mail-handler">
-                    <button class="minimize" title="Minimize">
+                    <button class="minimize" title="Minimize" @click="switchSize('minimum')">
                         <img :src="minimizeCmp" alt="" />
                     </button>
-                    <button class="full-screen" title="Full Screen (shift for pop-out)">
+                    <button class="full-screen" :title="fullScreenTitle" 
+                    @click="switchSize('toggle')">
                         <img :src="shiftSize" alt="" />
                     </button>
                     <button class="save-and-close" title="Save & Close" @click="saveAndClose">
@@ -18,22 +19,25 @@ export default {
                     </button>
                 </div>
             </header>
-            <div class="send-to" @mouseleave="handleConcealment('null')" 
-            :class="{twocells: hasTwoRecipient, threecells: hasThreeRecipient}"> 
-                <div class="recipient-placeholder" :class="{conceal: hasSubject}">Recipients</div>
-                <div class="to-txt" :class="{conceal: isToTxtConceal}">To:</div>
-                <input class="target-input recipient" v-model="newMailData.subject" type="text" 
-                @focus="handleConcealment('focus')"/>
-                <button class="add-copy btn" @click="handleConcealment('copy')" 
-                :class="{conceal: !isCopyConceal, conceal: isToTxtConceal}">Cc</button>
-                <button class="add-blind-copy btn" @click="handleConcealment('blind')" 
-                :class="{conceal: !isBlindConceal, conceal: isToTxtConceal}">Bcc</button>
-                <div class="cc-txt" :class="{conceal: isCopyConceal}">Cc:</div>
-                <input class="copy-input recipient" :class="{conceal: isCopyConceal}" type="text"/>
-                <div class="bcc-txt" :class="{conceal: isBlindConceal}">Bcc:</div>
-                <input class="blind-input recipient" :class="{conceal: isBlindConceal}" type="text"/>
-            </div>
-            <div class="mail-subject"></div>
+            <section class="new-mail-body">
+                <div class="send-to" @mouseleave="handleConcealment('null')" 
+                :class="{twocells: hasTwoRecipient, threecells: hasThreeRecipient}"> 
+                    <div class="recipient-placeholder" :class="{conceal: hasSubject}">Recipients</div>
+                    <div class="to-txt" :class="{conceal: isToTxtConceal}">To:</div>
+                    <input class="target-input recipient" v-model="newMailData.subject" type="text" 
+                    @focus="handleConcealment('focus')"/>
+                    <button class="add-copy btn" @click="handleConcealment('copy')" 
+                    :class="{conceal: !isCopyConceal, conceal: isToTxtConceal}">Cc</button>
+                    <button class="add-blind-copy btn" @click="handleConcealment('blind')" 
+                    :class="{conceal: !isBlindConceal, conceal: isToTxtConceal}">Bcc</button>
+                    <div class="cc-txt" :class="{conceal: isCopyConceal}">Cc:</div>
+                    <input class="copy-input recipient" :class="{conceal: isCopyConceal}" type="text"/>
+                    <div class="bcc-txt" :class="{conceal: isBlindConceal}">Bcc:</div>
+                    <input class="blind-input recipient" :class="{conceal: isBlindConceal}" type="text"/>
+                </div>
+                <div class="mail-subject"></div>
+            </section>
+            
         </section>
     `, 
     data(){
@@ -44,6 +48,8 @@ export default {
             isBlindConceal: true,
             hasTwoRecipient: false,
             hasThreeRecipient: false,
+            composerSize: 'small',
+            fullScreenTitle: 'Full Screen (shift for pop-out)',
             minimizeCmp: null,
             shiftSize: null,
             closeX: null,
@@ -73,6 +79,35 @@ export default {
             clearInterval(this.saveIntervalId);
             this.$emit('saveAndClose',this.newMailData);
             
+        },
+        switchSize(value){
+            if(value === 'toggle'){
+                if(this.composerSize === 'small'){
+                    this.composerSize = 'large';
+                } else if (this.composerSize === 'large'){
+                    this.composerSize = 'small';
+                } else {
+                     /* this.composerSize === minimum*/
+                     this.composerSize = 'large';
+                }
+            }else{
+                 /* value === minimum*/
+                this.composerSize = value;
+            }
+            
+            const currRef = this.$refs.composerContainer;
+            currRef.classList.remove('minimum'); 
+            currRef.classList.remove('small');
+            currRef.classList.remove('large');
+
+            if(this.composerSize === 'small'){
+                currRef.classList.add('small');
+            } else if(this.composerSize === 'large'){
+                currRef.classList.add('large');
+            } else {
+                /* this.composerSize === minimum*/
+                currRef.classList.add('minimum');
+            }
         },
         handleConcealment(activator){
             switch(activator){
