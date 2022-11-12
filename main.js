8c43039df10b71cead0323f0e1a470edde379178
ohad bergmann <ohad.bergmann@gmail.com>
2022-11-12 23:58:12 +0200

    fixing git hub page 2.6

--- main.js
+++ main.js
@@ -24,4 +24,10 @@ const options = {
 
 const app = createApp(options)
 app.use(router)
+app.use("/apps", express.static('./apps/'));
+app.use("/assets", express.static('./assets/'));
+app.use("/cmps", express.static('./cmps/'));
+app.use("/lib", express.static('./lib/'));
+app.use("/services", express.static('./services/'));
+app.use("/views", express.static('./views/'));
 app.mount('#app')
